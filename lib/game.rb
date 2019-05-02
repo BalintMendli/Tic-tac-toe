626d38a0dc298e52debee476768a83b9aee3afad
zenott <mblevel@gmail.com>
2019-05-03 00:56:35 +0200

    Add win condition

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require_relative './player.rb'
 require_relative './board.rb'
 require_relative './ui.rb'
+require 'set'
 
 class Game
   include UI
@@ -11,17 +12,17 @@ class Game
     2.times { |n| get_player_data(n) }
     @current_player = @players[0]
     @other_player = @players[1]
-    while !won? && !full?
+    until full?
       clear_console
-      output(players_info(),true)
-      board.draw()
-      move=ask_move(@current_player)
+      output(players_info, true)
+      board.draw
+      abort("#{@players[won].name} won the game!") if won
+      move = ask_move(@current_player)
       @current_player.add_position(move)
-      board.update(@players[0].positions,@players[0].sign,@players[1].positions,@players[1].sign)
-      switch_players()
-      gets
+      board.update(@players[0].positions, @players[0].sign, @players[1].positions, @players[1].sign)
+      switch_players
     end
-    abort("The game ended with a draw!")  
+    abort('The game ended with a draw!')
   end
 
   def get_player_data(n)
@@ -44,7 +45,7 @@ class Game
     output("#{player.name}'s move", true)
     output('Enter your next move (1-9): ')
     move = input.to_i
-    while !valid_move?(move)
+    until valid_move?(move)
       output('Invalid move. Try again: ')
       move = input.to_i
     end
@@ -52,15 +53,18 @@ class Game
   end
 
   def valid_move?(move)
-    move>0 && move<10 && !@current_player.positions.include?(move) && !@other_player.positions.include?(move)
+    move > 0 && move < 10 && !@current_player.positions.include?(move) && !@other_player.positions.include?(move)
   end
 
-  def won?
-    false
+  def won
+    winning_sets = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    winning_sets.each { |set| return 0 if set.to_set.subset?(@players[0].positions.to_set) }
+    winning_sets.each { |set| return 1 if set.to_set.subset?(@players[1].positions.to_set) }
+    nil
   end
 
   def full?
-     (@players[0].positions + @players[1].positions).length
+    (@players[0].positions + @players[1].positions).length
     (@players[0].positions + @players[1].positions).length == 9
   end
 end
